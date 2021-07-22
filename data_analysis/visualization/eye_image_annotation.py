fc373361a7589c5da5c893d32625ea234a2b2f1a
kamran <kamranbinaee@gmail.com>
2021-07-22 14:10:50 -0700

    stable version for pupil and iris annotation (eyelids not included in this version)

--- data_analysis/visualization/eye_image_annotation.py
+++ data_analysis/visualization/eye_image_annotation.py
@@ -4,13 +4,21 @@
 
 
 def _get_annotation_path_from_image_path(image_file_name, path, eye_part):
-    print('looking for: ', path + image_file_name + eye_part + '.txt')
+    # print('get txt file name: ', path + image_file_name + eye_part + '.txt')
     return path + image_file_name + eye_part + '.txt'
 
 
 def fit_pupil(image_path, saving_directory, curr_image_number, plot=False, write_annotation=False, eye_part='pupil'):
-    save_directory = "/home/kamran/Downloads/eye_image_annotation_results/"
 
+    # Mouse enumeration for development use only
+    # Todo: Remove this after we're done with the design
+    '''
+    BACK = 8
+    FORWARD = 9
+    LEFT = 1
+    MIDDLE = 2
+    RIGHT = 3
+    '''
     if 'pupil' in eye_part:
         point_color = 'yellow'
         fill_color = 'orange'
@@ -18,29 +26,31 @@ def fit_pupil(image_path, saving_directory, curr_image_number, plot=False, write
         point_color = 'blue'
         fill_color = 'cyan'
 
-    image_file_name = os.path.basename(image_path)
-
+    base = os.path.basename(image_path)
+    image_file_name = os.path.splitext(base)[0]
+    result = 'success'
     while True:
         plt.ion()
         fig, ax = plt.subplots(figsize=(15, 15))
         img = imread(image_path)
-        ax.set_title('({}): {}'.format(curr_image_number, os.path.basename(image_path)))
+        ax.set_title('ID:{} File Name:{}'.format(curr_image_number, os.path.basename(image_path)))
         ax.imshow(img, cmap='gray')
 
         key_points = plt.ginput(-1, mouse_pop=2, mouse_stop=3,
                                 timeout=-1)  # If negative, accumulate clicks until the input is terminated manually.
 
         if not key_points:
-            if write_annotation:
-                annotated_text_file_name = _get_annotation_path_from_image_path(image_file_name, saving_directory,
-                                                                                eye_part)
-                with open(annotated_text_file_name, 'w+') as f:
-                    f.write("closed_eye")
-
-                with open(annotated_text_file_name.replace(".txt","_points.txt"),
-                          'w+') as f:  # For detecting selected
-                    f.write("closed_eye")
+            # if write_annotation:
+            #     annotated_text_file_name = _get_annotation_path_from_image_path(image_file_name, saving_directory,
+            #                                                                     eye_part)
+            #     with open(annotated_text_file_name, 'w+') as f:
+            #         f.write("closed_eye")
+            #
+            #     with open(annotated_text_file_name.replace(".txt", "_points.txt"),
+            #               'w+') as f:  # For detecting selected
+            #         f.write("closed_eye")
             plt.close()
+            result = 'quit'
             break
 
         fitted = LSqEllipse()
@@ -56,12 +66,12 @@ def fit_pupil(image_path, saving_directory, curr_image_number, plot=False, write
                 if all([c <= 50 for c in center_coord]):
                     points_str = '-1:-1'
                 else:
-                    points_str = '{}:{}'.format(center_coord[0], center_coord[1])
+                    points_str = '{}, {}'.format(center_coord[0], center_coord[1])
                 f.write(points_str)
 
             with open(annotated_text_file_name.replace(".txt","_points.txt"), 'w+') as f:  # For detecting selected
                 for point in key_points:
-                    f.write('{}:{}\n'.format(point[0], point[1]))
+                    f.write('{}, {}\n'.format(point[0], point[1]))
 
         if plot:
             # ax.annotate('pred center', xy=center_coord, xycoords='data',
@@ -84,29 +94,33 @@ def fit_pupil(image_path, saving_directory, curr_image_number, plot=False, write
                                 os.path.splitext(os.path.basename(image_path))[0] + eye_part + "_ellipse.png"
             output_image_file = saving_directory + image_file_name + eye_part + "_ellipse.png"
             fig.savefig(output_image_file)
-            print("saved: ", output_image_file)
+            print("saved: ", os.path.basename(output_image_file))
             plt.show()
             confirmation_point = plt.ginput(1, timeout=-1, mouse_add=3, mouse_stop=3)
             plt.close()
             if len(confirmation_point) == 0:
                 break
+    return result
 
 
 def annotate(image_directory, saving_directory, eye_part='pupil'):
     images_paths = sorted(glob(os.path.join(image_directory, '*png')))
     # imag_paths = sorted(glob(os.path.join(base_dir, '*jpg')))
     annotation_paths = glob(os.path.join(saving_directory, '*txt'))
-    i = 1
+    i = 0
     for image_path in images_paths:
-        print("Running Annotation for: ", image_path)
-        image_file_name = os.path.basename(image_path)
+        print("Running Annotation for: {} ID: {}/{}".format(os.path.basename(image_path), i, len(images_paths)))
+        base = os.path.basename(image_path)
+        image_file_name = os.path.splitext(base)[0]
         annotated_text_file_name = _get_annotation_path_from_image_path(image_file_name, saving_directory, eye_part)
         if annotated_text_file_name in annotation_paths:
-            print("Found the existing txt file for: ", annotated_text_file_name)
-            continue
+            print("Found the existing txt file for: ", os.path.basename(annotated_text_file_name))
         else:
-            fit_pupil(image_path, saving_directory=saving_directory, curr_image_number=i, plot=True, write_annotation=True, eye_part=eye_part)
-            i += 1
+            result = fit_pupil(image_path=image_path, saving_directory=saving_directory, curr_image_number=i, plot=True, write_annotation=True, eye_part=eye_part)
+            if result is not 'success':
+                print("\n\nQuit!!\n\n")
+                break
+        i = i + 1
 
     # for imag_path in imag_paths:
     #     if _get_annotation_path_from_image_path(imag_path, eye_part) in annotation_paths:
