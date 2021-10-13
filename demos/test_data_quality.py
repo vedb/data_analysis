8c255a832ad194d5513cee05f6ec9d6ab4183950
kamran <kamranbinaee@gmail.com>
2021-10-12 23:39:06 -0700

    Post-collection data quality script (stable)

--- demos/test_data_quality.py
+++ demos/test_data_quality.py
@@ -10,6 +10,7 @@ from pupil_recording_interface.externals.circle_detector import find_pupil_circl
 import matplotlib.pyplot as plt
 from pathlib import Path
 import yaml
+import copy
 
 class session:
     """ A light Session class to work with different streams of data in a recording session """
@@ -116,16 +117,37 @@ def parse_pipeline_parameters(parameters_fpath):
         param_dict = yaml.safe_load(stream)
     return param_dict
 
+def plot_marker_images(mean_image_cal, mean_image_val, path):
+    fig, axs = plt.subplots(nrows=1, ncols=2)
+    fig.set_figheight(10)
+    fig.set_figwidth(10)
+    axs[0].set_title("Calibration Average Image", fontsize=18)
+    axs[0].imshow(mean_image_cal.astype(int))
+    axs[0].tick_params(axis='x', labelsize=14)
+    axs[0].tick_params(axis='y', labelsize=14)
+    axs[1].set_title("Validation Average Image", fontsize=18)
+    axs[1].imshow(mean_image_val.astype(int))
+    axs[1].tick_params(axis='x', labelsize=14)
+    axs[1].tick_params(axis='y', labelsize=14)
+    saving_directory = path + "/data_quality_checks"
+    file_name = os.path.join(saving_directory, "average_marker_images.png")
+    print("Saving image to: {}".format(file_name))
+    Path(saving_directory).mkdir(parents=True, exist_ok=True)
+    plt.savefig(file_name, dpi=120, facecolor="white")
+    plt.show()
+    plt.close('all')
+
+
 def plot_average_eye_images(mean_image_0, mean_image_1, path):
     fig, axs = plt.subplots(nrows=1, ncols=2)
     fig.set_figheight(10)
     fig.set_figwidth(10)
     axs[0].set_title("Right Eye Average Image", fontsize=18)
-    axs[0].imshow(mean_image_0, cmap='Greys')
+    axs[0].imshow(mean_image_0.astype(int), cmap='gray')
     axs[0].tick_params(axis='x', labelsize=14)
     axs[0].tick_params(axis='y', labelsize=14)
     axs[1].set_title("Left Eye Average Image", fontsize=18)
-    axs[1].imshow(mean_image_1, cmap='Greys')
+    axs[1].imshow(mean_image_1.astype(int), cmap='gray')
     axs[1].tick_params(axis='x', labelsize=14)
     axs[1].tick_params(axis='y', labelsize=14)
     saving_directory = path + "/data_quality_checks"
@@ -208,7 +230,7 @@ def plot_marker_position(world_dataframe_cal, world_dataframe_val, path):
     fig.set_figheight(10)
     fig.set_figwidth(10)
     axs[0].set_title("Calibration Marker Positions", fontsize=18)
-    axs[0].scatter(world_dataframe_cal["cal_norm_pos_x"], world_dataframe_cal["cal_norm_pos_y"], c="orange",
+    axs[0].scatter(world_dataframe_cal["cal_norm_pos_x"], 1 - world_dataframe_cal["cal_norm_pos_y"], c="orange",
                    edgecolors='k', s=58, alpha=0.9)
     axs[0].grid(True)
     axs[0].set_xlim([0, 1])
@@ -220,7 +242,7 @@ def plot_marker_position(world_dataframe_cal, world_dataframe_val, path):
     axs[0].set_ylabel("Norm Pixel Y", fontsize=12)
 
     axs[1].set_title("Validation Marker Positions", fontsize=18)
-    axs[1].scatter(world_dataframe_val["val_norm_pos_x"], world_dataframe_val["val_norm_pos_y"], c="cyan",
+    axs[1].scatter(world_dataframe_val["val_norm_pos_x"], 1 - world_dataframe_val["val_norm_pos_y"], c="cyan",
                    edgecolors='k', s=58, alpha=0.9)
     axs[1].grid(True)
     axs[1].set_xlim([0, 1])
@@ -238,11 +260,14 @@ def plot_marker_position(world_dataframe_cal, world_dataframe_val, path):
     plt.show()
     plt.close('all')
 
-def plot_data_quality(session, pupil_dataframe, mean_image_0, mean_image_1, world_dataframe_cal, world_dataframe_val):
-    plot_average_eye_images(mean_image_0, mean_image_1, session.session_path)
-    plot_pupil_confidence(pupil_dataframe, session.session_path)
-    plot_all_fps(session)
-    plot_marker_position(world_dataframe_cal, world_dataframe_val, session.session_path)
+def plot_data_quality(my_session, pupil_dataframe, mean_image_0, mean_image_1,
+                      world_dataframe_cal, mean_image_cal,
+                      world_dataframe_val, mean_image_val):
+    plot_average_eye_images(mean_image_0, mean_image_1, my_session.session_path)
+    plot_pupil_confidence(pupil_dataframe, my_session.session_path)
+    plot_all_fps(my_session)
+    plot_marker_position(world_dataframe_cal, world_dataframe_val, my_session.session_path)
+    plot_marker_images(mean_image_cal, mean_image_val, my_session.session_path)
 
 def run_pupil_detection_PL(session, number_of_frames=250):
     pupil_dict_keys = ["pupil_timestamp", "eye_index", "world_index", "eye_id", "confidence", "norm_pos_x",
@@ -346,6 +371,8 @@ def run_marker_detection_0(session, frame_range=(0,500), skip_frame=4, scale=0.5
     world_frame_size = session.video_frame_size_cv2(world_video)
     world_timestamp = session.read_timestamp_np("world")
 
+    boundary = 50
+    prev_frame = None
     properties = {}
     detection_method = "2d-pupil-labs"
 
@@ -353,8 +380,13 @@ def run_marker_detection_0(session, frame_range=(0,500), skip_frame=4, scale=0.5
     world_image_indexes = np.arange(frame_range[0], frame_range[1], skip_frame)
     df = pd.DataFrame(columns=world_dict_keys, index=world_image_indexes, dtype=np.float64)
     print("Running calibration marker detection for {} world video frames".format(len(world_image_indexes)))
-    count = 0
+    hdim = world_frame_size[0]
+    vdim = world_frame_size[1]
+    a = np.zeros((int(vdim*scale), int(hdim*scale), 3), dtype=np.float64) + 120
+    current_frame = copy.deepcopy(a)
+    current_frame.setflags(write=1)
     t = tqdm(world_image_indexes, desc="Frame Count", leave=True)
+    count = t.iterable[0]
     for frame_index in t:
 
         world_video.set(1, frame_index)
@@ -365,27 +397,33 @@ def run_marker_detection_0(session, frame_range=(0,500), skip_frame=4, scale=0.5
             img = cv2.resize(img, None, fx=scale, fy=scale)
 
         frame = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        vdim, hdim = frame.shape[:2]
-
+        frame_3_channel = np.stack((frame,) * 3, axis=-1)
         world_circles = find_pupil_circle_marker(frame, scale)
         for iw, w in enumerate(world_circles):
             ellipses = w["ellipses"]
             for e in ellipses:
-                this_circle = {}
                 ellipse_centers = np.array([e[0] for e in ellipses])
-                df.loc[count, "cal_location_x"] = ellipse_centers.mean(0).tolist()[0]
-                df.loc[count, "cal_location_y"] = ellipse_centers.mean(0).tolist()[1]
-                df.loc[count, "cal_norm_pos_x"] = ellipse_centers.mean(0).tolist()[0] / hdim
-                df.loc[count, "cal_norm_pos_y"] = ellipse_centers.mean(0).tolist()[1] / vdim
+                x = ellipse_centers.mean(0).tolist()[0]
+                y = ellipse_centers.mean(0).tolist()[1]
+                df.loc[count, "cal_location_x"] = x / scale
+                df.loc[count, "cal_location_y"] = y / scale
+                df.loc[count, "cal_norm_pos_x"] = x / (scale * hdim)
+                df.loc[count, "cal_norm_pos_y"] = y / (scale * vdim)
 
                 ellipse_radii = np.array([e[1] for e in ellipses])
                 df.loc[count, "cal_size"] = ellipse_radii.max(0)[0]
                 df.loc[count, "world_timestamp"] = world_timestamp[frame_index]
+                df.loc[count, "world_index"] = frame_index
 
-                ref_pixel_x = int(df.loc[count, "cal_location_x"])
-                ref_pixel_y = int(df.loc[count, "cal_location_y"])
+                ref_pixel_x = int(df.loc[count, "cal_location_x"] * scale)
+                ref_pixel_y = int((df.loc[count, "cal_location_y"]) * scale)
                 df.loc[count, "method"] = detection_method
-                img = cv2.circle(img, (ref_pixel_x, ref_pixel_y), 8, (200, 25, 205), 2)
+                img = cv2.circle(frame_3_channel, (ref_pixel_x, ref_pixel_y), 8, (200, 25, 205), 2)
+                range_x = np.arange(max(0, ref_pixel_x - boundary), min(int(scale * hdim) - 1, ref_pixel_x + boundary))
+                range_y = np.arange(max(0, ref_pixel_y - boundary), min(int(scale * vdim) - 1, ref_pixel_y + boundary))
+                # print(range_x,range_y)
+                pp = img[range_y.min():range_y.max()][:, range_x.min():range_x.max()]
+                current_frame[range_y.min():range_y.max()][:, range_x.min():range_x.max()] = pp
                 frame = img
                 count = count + 1
 
@@ -394,19 +432,10 @@ def run_marker_detection_0(session, frame_range=(0,500), skip_frame=4, scale=0.5
         cv2.imshow('Scene Circle Detection', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    # # print('\nDone!')
-    # cv2.destroyAllWindows()
-    # # print("timestamp shape: ", np.asarray(times).shape)
-    # print("locations shape: ", np.asarray(locations).shape)
-    # print("mean locations shape: ", np.asarray(mean_locations).shape)
-    # print("norm pos shape: ", np.asarray(norm_pos).shape)
-    # print("mean norm pos shape: ", np.asarray(mean_norm_pos).shape)
-    # print("loc: ", mean_locations)
-    # print("norm: ", mean_norm_pos)
     session.close_video_cv2("world")
     cv2.destroyAllWindows()
     print("\nDone!")
-    return df
+    return df, current_frame
 
 
 def run_marker_detection_1(session, frame_range=(0, 500), skip_frame=4, scale=0.5):
@@ -414,7 +443,7 @@ def run_marker_detection_1(session, frame_range=(0, 500), skip_frame=4, scale=0.
 
     checkerboard_size = (6, 8)
     rows, cols = checkerboard_size
-    # termination criteria: Make inputs?
+    # TODO: termination criteria: Make inputs?
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     world_dict_keys = ["world_timestamp", "world_index", "eye_index", "method",
@@ -426,6 +455,8 @@ def run_marker_detection_1(session, frame_range=(0, 500), skip_frame=4, scale=0.
     world_frame_size = session.video_frame_size_cv2(world_video)
     world_timestamp = session.read_timestamp_np("world")
 
+    boundary = 50
+    prev_frame = None
     properties = {}
     detection_method = "2d-opencv-v0"
 
@@ -433,8 +464,14 @@ def run_marker_detection_1(session, frame_range=(0, 500), skip_frame=4, scale=0.
     world_image_indexes = np.arange(frame_range[0], frame_range[1], skip_frame)
     df = pd.DataFrame(columns=world_dict_keys, index=world_image_indexes, dtype=np.float64)
     print("Running validation marker detection for {} world video frames".format(len(world_image_indexes)))
-    count = 0
+    hdim = world_frame_size[0]
+    vdim = world_frame_size[1]
     t = tqdm(world_image_indexes, desc="Frame Count", leave=True)
+    count = t.iterable[0]
+    a = np.zeros((int(vdim*scale), int(hdim*scale), 3), dtype=np.float64) + 120
+    current_frame = copy.deepcopy(a)
+    current_frame.setflags(write=1)
+
     for frame_index in t:
 
         world_video.set(1, frame_index)
@@ -445,7 +482,7 @@ def run_marker_detection_1(session, frame_range=(0, 500), skip_frame=4, scale=0.
             img = cv2.resize(img, None, fx=scale, fy=scale)
 
         frame = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        vdim, hdim = frame.shape[:2]
+        frame_3_channel = np.stack((frame,) * 3, axis=-1)
 
         # Find the chess board corners
         ret, corners = cv2.findChessboardCorners(frame, checkerboard_size, None)
@@ -455,7 +492,7 @@ def run_marker_detection_1(session, frame_range=(0, 500), skip_frame=4, scale=0.
             corners2 = cv2.cornerSubPix(frame, corners, (11, 11), (-1, -1), criteria)
             corners = np.squeeze(corners2)
             # Draw and display the corners
-            frame = cv2.drawChessboardCorners(img, (6, 8), corners, ret)
+            frame_3_channel = cv2.drawChessboardCorners(frame_3_channel, (6, 8), corners, ret)
             corners[:, 0] = corners[:, 0] * (1 / scale)
             corners[:, 1] = corners[:, 1] * (1 / scale)
             marker_position = np.mean(corners, axis=0)
@@ -464,31 +501,33 @@ def run_marker_detection_1(session, frame_range=(0, 500), skip_frame=4, scale=0.
             df.loc[count, "method"] = detection_method
             df.loc[count, "val_location_x"] = marker_position[0]
             df.loc[count, "val_location_y"] = marker_position[1]
-            corners[:, 0] = corners[:, 0] * (scale / hdim)
-            corners[:, 1] = corners[:, 1] * (scale / vdim)
+
+            ref_pixel_x = int(marker_position[0]*scale)
+            ref_pixel_y = int(marker_position[1]*scale)
+            frame_3_channel = cv2.circle(frame_3_channel,
+                               (int(marker_position[0]*scale), int(marker_position[1]*scale)),
+                               8, (25, 200, 205), 2)
+            corners[:, 0] = corners[:, 0] * (1 / hdim)
+            corners[:, 1] = corners[:, 1] * (1 / vdim)
             marker_position = np.mean(corners, axis=0)
             df.loc[count, "val_norm_pos_x"] = marker_position[0]
             df.loc[count, "val_norm_pos_y"] = marker_position[1]
+            range_x = np.arange(max(0, ref_pixel_x - boundary), min(int(scale * hdim)-1, ref_pixel_x + boundary))
+            range_y = np.arange(max(0, ref_pixel_y - boundary), min(int(scale * vdim)-1, ref_pixel_y + boundary))
+            pp = frame_3_channel[range_y.min():range_y.max()][:, range_x.min():range_x.max()]
+            current_frame[range_y.min():range_y.max()][:, range_x.min():range_x.max()] = pp
             count = count + 1
+            frame = frame_3_channel
 
         t.set_description("Frame Count (Found %i)" % ret)
         t.refresh()  # to show immediately the update
         cv2.imshow('Scene Checkerboard Detection', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    # # print('\nDone!')
-    # cv2.destroyAllWindows()
-    # # print("timestamp shape: ", np.asarray(times).shape)
-    # print("locations shape: ", np.asarray(locations).shape)
-    # print("mean locations shape: ", np.asarray(mean_locations).shape)
-    # print("norm pos shape: ", np.asarray(norm_pos).shape)
-    # print("mean norm pos shape: ", np.asarray(mean_norm_pos).shape)
-    # print("loc: ", mean_locations)
-    # print("norm: ", mean_norm_pos)
     session.close_video_cv2("world")
     cv2.destroyAllWindows()
     print("\nDone!")
-    return df
+    return df, current_frame
 
 if __name__ == "__main__":
 
@@ -508,7 +547,9 @@ if __name__ == "__main__":
     (minutes, seconds) = tuple(map(int, raw_input.split(',')))
     end_index = 60 * 30 * int(minutes) + 30 * int(seconds)
     scale = 0.5
-    world_dataframe_cal = run_marker_detection_0(session=my_session, frame_range=(start_index, end_index), skip_frame=15, scale=scale)
+    world_dataframe_cal, mean_image_cal = run_marker_detection_0(session=my_session,
+                                                                 frame_range=(start_index, end_index),
+                                                                 skip_frame=15, scale=scale)
 
     # Fix me: Something goes wrong with the video closing that the session needs to be re-instantiated
     my_session = session(session_path)
@@ -520,9 +561,13 @@ if __name__ == "__main__":
     (minutes, seconds) = tuple(map(int, raw_input.split(',')))
     end_index = 60 * 30 * int(minutes) + 30 * int(seconds)
     scale = 0.5
-    world_dataframe_val = run_marker_detection_1(session=my_session, frame_range=(start_index, end_index), skip_frame=15, scale=scale)
+    world_dataframe_val, mean_image_val = run_marker_detection_1(session=my_session,
+                                                                 frame_range=(start_index, end_index),
+                                                                 skip_frame=15, scale=scale)
 
     # Fix me: Something goes wrong with the video closing that the session needs to be re-instantiated
     my_session = session(session_path)
     my_session.read_all_timestamps()
-    plot_data_quality(my_session, pupil_dataframe, mean_image_0, mean_image_1, world_dataframe_cal, world_dataframe_val)
+    plot_data_quality(my_session, pupil_dataframe, mean_image_0, mean_image_1,
+                      world_dataframe_cal, mean_image_cal,
+                      world_dataframe_val, mean_image_val)
