43172f231808af31ba7f1ca079927660e8f7a735
kamran <kamranbinaee@gmail.com>
2021-10-27 18:07:03 -0700

    created the complete dataset using all_masks.py

--- data_analysis/visualization/all_masks.py
+++ data_analysis/visualization/all_masks.py
@@ -9,8 +9,8 @@ import copy
 
 def create_label(raw_image_path, label_image_path, param_dict):
 
-    saving_directory_mask = param_dict['directory']['saving_directory_mask']
-    saving_directory_array = param_dict['directory']['saving_directory_array']
+    saving_directory_mask = label_image_path.replace("all_labels", "all_masks")
+    saving_directory_array = label_image_path.replace("all_labels", "all_arrays")
     raw_image = cv2.imread(raw_image_path)
     label_image = cv2.imread(label_image_path)
     final_mask = np.zeros(label_image.shape, dtype=np.uint8)
@@ -25,46 +25,14 @@ def create_label(raw_image_path, label_image_path, param_dict):
                 mask_color[i][j] = [0, 0, 0]
                 mask_array[i][j] = 0
             elif np.array_equal(label_image[i][j][:], [255, 255, 0]):
-                mask_color[i][j] = [50, 50, 50]
+                mask_color[i][j] = [75, 75, 75]
                 mask_array[i][j] = 1
             elif np.array_equal(label_image[i][j][:], [255, 0, 0]):
-                mask_color[i][j] = [100, 100, 100]
+                mask_color[i][j] = [150, 150, 150]
                 mask_array[i][j] = 2
             elif np.array_equal(label_image[i][j][:], [0, 0, 255]):
-                mask_color[i][j] = [200, 200, 200]
+                mask_color[i][j] = [225, 225, 225]
                 mask_array[i][j] = 3
-    '''
-    # final_mask = copy.deepcopy(label_image)
-    # final_mask.setflags(write=1)
-    # final_mask[label_image == [0, 0, 0]] = 0 # [0, 0, 0]
-    # idx = np.where(label_image[:,:,0] == 128 and label_image[:,:,1] == 128 and label_image[:,:,2] == 128)
-    idx = np.where(label_image == [128, 128, 128])
-    final_mask = np.where(label_image[:,:] == [128, 128, 128], final_mask, [0,0,0])
-    # c = np.tile([0.,0.,0.],[idx.shape[0],idx.shape[1],1])
-    # print("Hi")
-    # final_mask[idx[0]][:,idx[1]] = c[idx[0]][:,idx[1]]
-    # idx = np.where(label_image[:,:,0] == 255 and label_image[:,:,1] == 255 and label_image[:,:,2] == 255)
-    idx = np.where(label_image == [255, 255, 255])
-    final_mask = np.where(label_image[:,:] == [255, 255, 255], final_mask, [0,0,0])
-    # c = np.tile([0.,0.,0.],[idx.shape[0],idx.shape[1],1])
-    # print("Hooo")
-    # final_mask[idx[0]][:,idx[1]] = c[idx[0]][:,idx[1]]
-    # idx = np.where(label_image[:,:,0] == 255 and label_image[:,:,1] == 255 and label_image[:,:,2] == 0)
-    idx = np.where(label_image == [255, 255, 0])
-    final_mask = np.where(label_image[:,:] == [255, 255, 0], final_mask, [50,50,50])
-    # c = np.tile([50.,50.,50.],[idx.shape[0],idx.shape[1],1])
-    # final_mask[idx[0]][:,idx[1]] = c[idx[0]][:,idx[1]] + 50
-    # idx = np.where(label_image[:,:,0] == 255 and label_image[:,:,1] == 0 and label_image[:,:,2] == 0)
-    idx = np.where(label_image == [255, 0, 0])
-    final_mask = np.where(label_image[:,:] == [255, 0, 0], final_mask, [100,100,100])
-    # c = np.tile([100., 100., 100.],[idx.shape[0],idx.shape[1],1])
-    # final_mask[idx[0]][:,idx[1]] = c[idx[0]][:,idx[1]] + 100
-    # idx = np.where(label_image[:,:,0] == 0 and label_image[:,:,1] == 0 and label_image[:,:,2] == 255)
-    idx = np.where(label_image == [0, 0, 255])
-    final_mask = np.where(label_image[:,:] == [0, 0, 255], final_mask, [200,200,200])
-    # c = np.tile([200., 200., 200.],[idx.shape[0],idx.shape[1],1])
-    # final_mask[idx[0]][:,idx[1]] = c[idx[0]][:,idx[1]] + 200
-    '''
     final_mask = mask_color
     colors = np.unique(label_image.reshape(400*400, 3), axis=0)
     tag = "{} regions:\n{}".format(len(colors), colors)
@@ -75,21 +43,25 @@ def create_label(raw_image_path, label_image_path, param_dict):
     # frame = np.concatenate((frame, final_mask), axis=1)
     cv2.imshow("Input & Label", frame)
     cv2.imshow("Mask", final_mask.astype(np.uint8))
-    base = os.path.basename(raw_image_path)
-    mask_file_name = os.path.splitext(base)[0]
-    file_name = saving_directory_mask + mask_file_name + "_mask.png"
+    base = os.path.basename(saving_directory_mask)
+    mask_file_name = os.path.splitext(base)[0].replace("_label", "_mask")
+    # print("mask: ", os.path.dirname(os.path.realpath(saving_directory_mask)), mask_file_name)
+    file_name = os.path.dirname(os.path.realpath(saving_directory_mask)) + "/" + mask_file_name + ".png"
     # print(file_name)
     cv2.imwrite(file_name, final_mask.astype(np.uint8))
-    file_name = saving_directory_array + mask_file_name + "_array.npy"
+    base = os.path.basename(saving_directory_array)
+    mask_file_name = os.path.splitext(base)[0].replace("_label", "_array")
+    # print("array: ", mask_file_name, os.path.dirname(os.path.realpath(saving_directory_array)), mask_file_name)
+    file_name = os.path.dirname(os.path.realpath(saving_directory_array)) + "/" + mask_file_name + ".npy"
     np.save(file_name, mask_array)
-    if cv2.waitKey(500) & 0xFF == ord("q"):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         return True
     # cv2.destroyAllWindows()
     return True
 
 
 def all_labels(image_directory, label_directory, param_dict):
-
+    result = False
     images_paths = sorted(glob(os.path.join(image_directory, '*png')))
     i = 0
     for image_path in images_paths:
@@ -97,13 +69,13 @@ def all_labels(image_directory, label_directory, param_dict):
         base = os.path.basename(image_path)
         image_file_name = os.path.splitext(base)[0]
         files_missing = []
-        label_image_file_name = label_directory + image_file_name + "_label.png"
-        input_label_image_file_name = label_directory + image_file_name + "_label___input.png"
+        label_image_file_name = label_directory + "/" + image_file_name + "_label.png"
+        input_label_image_file_name = label_directory + "/" + image_file_name + "_label_&_input.png"
         image_files = [label_image_file_name, input_label_image_file_name]
         for image_file in image_files:
             if not os.path.exists(image_file):
                 files_missing.append(os.path.basename(image_file))
-                print("Image File {} not found!".format(txt_file))
+                print("Image File {} not found!".format(image_file))
         if len(files_missing) > 0:
             print("One or more file missing!!", files_missing)
             continue
@@ -129,9 +101,26 @@ if __name__ == '__main__':
     parameters_fpath = os.getcwd() + "/annotation_parameters.yaml"
     param_dict = parse_pipeline_parameters(parameters_fpath)
     image_directory = param_dict['directory']['image_directory']
-    label_directory = param_dict['directory']['label_directory']
+    parent_label_directory = param_dict['directory']['label_directory']
     print(param_dict)
-    all_labels(image_directory=image_directory,
-               label_directory=label_directory,
-               param_dict=param_dict)
+    parent_directory = "/hdd01/eye_images/Final_eye_images/Bates_labels/all_labels/"
+    directory_contents = ["/hdd01/eye_images/2021_07_05_13_09_00/",
+                          "/hdd01/eye_images/2021_07_13_11_34_04/",
+                          "/hdd01/eye_images/2021_07_13_14_32_23/",
+                          "/hdd01/eye_images/2021_07_13_14_37_32/",
+                          "/hdd01/eye_images/2021_07_13_14_43_36/",
+                          "/hdd01/eye_images/2021_07_14_17_00_32/",
+                          "/hdd01/eye_images/2021_07_16_12_29_06/",
+                          "/hdd01/eye_images/2021_07_16_13_00_43/",
+                          "/hdd01/eye_images/2021_07_21_13_47_43/",
+                          "/hdd01/eye_images/2021_07_22_14_04_09/",
+                          "/hdd01/eye_images/2021_07_22_14_49_47/",
+                          "/hdd01/eye_images/2021_07_22_15_25_59/"
+                          ]
+    for image_directory in directory_contents:
+        label_directory = parent_label_directory + os.path.basename(os.path.normpath(image_directory))
+        print("Label Directory:", label_directory)
+        all_labels(image_directory=image_directory,
+                   label_directory=label_directory,
+                   param_dict=param_dict)
     sys.exit(0)
