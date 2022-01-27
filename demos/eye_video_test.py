a5973a2aee49c9f9417a4b33b635f365aa66b55a
csinnott91 <csinnott@nevada.unr.edu>
2022-01-26 16:38:28 -0800

    Updated test script, can access controls once given frame is closed.

--- demos/eye_video_test.py
+++ demos/eye_video_test.py
@@ -17,7 +17,7 @@ Usage: python eye_video_test.py [None] [None]
 
 @author: KamranBinaee
 """
-
+import matplotlib.pyplot as plt
 import pupil_recording_interface as pri
 import pandas as pd
 import numpy as np
@@ -25,6 +25,10 @@ import os
 import cv2
 import sys
 import logging
+from pupil_recording_interface.device.video import (
+    VideoDeviceUVC,
+)
+
 
 # %%
 # Set Pupil Core generation
@@ -81,19 +85,55 @@ updated_dict = {
                    "Contrast": 10,
                    "Gamma": 100
 }
-with pri.StreamManager(configs) as manager:
-    while not manager.stopped:
-        if manager.all_streams_running:
-            status = manager.format_status(
-                "fps", format="{:.2f} Hz", max_cols=72, sleep=0.1
-            )
-            print("\r" + status, end="")
-        if manager.keypresses._getvalue():
-            key = manager.keypresses.popleft()
-            if key.lower() == "t":
-                # print(manager.streams["eye0"].device.capture)
-                # print(type(manager.streams["eye0"].device.capture))
-                # eye0_capture = manager.streams["eye0"].device.get_capture(f"Pupil Cam2 ID0", (400,400), 60)
-                manager.streams["eye0"].device.controls(updated_dict)
+change_control = False
+# with pri.StreamManager(configs) as manager:
+#     while not manager.stopped:
+#         if manager.keypresses._getvalue():
+#             key = manager.keypresses.popleft()
+#             if key.lower() == "t":
+#                 change_control = True
+#
+#         if manager.all_streams_running:
+#             status = manager.format_status(
+#                 "fps", format="{:.2f} Hz", max_cols=72, sleep=0.1
+#             )
+#             print("\r" + status, end="")
+#             if change_control:
+#                 print("\n\nt pressed!!")
+#                 # manager.streams["eye0"].device.controls = {"Gamma":100}
+#                 change_control = False
+#                 print(type(manager.streams["eye0"].device.controls))
+#                 # print(manager.streams["eye1"].device)
+#                 # print(type(manager.streams["eye0"].device.capture))
+#                 # eye0_capture = manager.streams["eye0"].device.get_capture(f"Pupil Cam2 ID0", (400,400), 60)
 
+device_uid = f"Pupil Cam{pupil_gen} ID0"
+resolution=(400, 400)
+fps = 60
+device = VideoDeviceUVC(device_uid, resolution, fps)
+
+# legal value
+with device:
+    device.controls = {"Gamma": 100}
+    device.controls = {"Gamma": 180}
+    assert device.controls["Gamma"] == 180
+print(device.controls["Gamma"])
+brightness = [60]
+gamma = [180, 199]
+contrast = [65]
+for br in brightness:
+    for g in gamma:
+        for c in contrast:
+            for i in range(10):
+                #, "Gamma": g, "Contrast":c}
+                frame, ts = device.get_frame_and_timestamp()
+                print(type(frame))
+                device.controls = {"Gamma": int(g)}
+                plt.pause(0.10)
+                # cv2.imshow(frame)
+                print("{} {} {}".format(br, g, c))
+                print("\r", end="")
+device.controls = {"Gamma":int(g)}
+print(device.controls["Gamma"])
+cv2.destroyAllWindows()
 print("\nStopped")
